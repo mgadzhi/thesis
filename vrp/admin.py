5b61eebf63b4a7e7fdc9a5ccf1c263bf407daa67
Gadzhi <gadzhi@home.(none)>
2013-04-02 01:49:35 +0400

    Django admin

---
+++ vrp/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from vrp import models
+
+__author__ = 'gadzhi'
+
+admin.site.register(models.Station)
+admin.site.register(models.Edge)
+admin.site.register(models.Network)
+admin.site.register(models.Vehicle)
+admin.site.register(models.Order)
