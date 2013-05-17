802500a6d9ab34f7dcbdc544a3ad050d0288c328
mgadzhi <musaevgadzhi@gmail.com>
2013-05-17 20:21:02 +0400

    Менюшки для админа, реселлера и агента. Безбожно криво прилеплены

--- vrp/urls.py
+++ vrp/urls.py
@@ -5,11 +5,11 @@ from vrp import views
 
 urlpatterns = patterns(
     '',
-    url(r'^/$', views.index),
+    url(r'^$', views.index),
     url(r'^orders/$', views.orders_list, name="orders_list"),
     url(r'^orders/create/$', views.order_create, name="create_order"),
     url(r'^orders/active-orders/$', views.active_orders, name="active_orders"),
-    url(r'^stations/$', views.stations_list),
+    url(r'^stations/$', views.stations_list, name="stations_list"),
     url(r'^stations/create/$', views.station_create, name="create_station"),
     url(r'^vehicles/$', views.vehicles_list, name="vehicles_list"),
     url(r'^vehicles/create/$', views.vehicle_create, name="create_vehicle"),
